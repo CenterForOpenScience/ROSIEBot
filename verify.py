2afbd59b6ffd2077bd5c49ad71de25c404e7f4b3
Matthew Keitelman <zamattiac@gmail.com>
2016-06-23 17:02:02 -0400

    Begin unittests, planned verification

--- verify.py
+++ verify.py
@@ -1,27 +1,57 @@
-import shelve # (temp)
+import json, codecs
 import os
 import sys
-# 1. Take in a list of pages at which to look.
+from settings import base_urls
+
+NUM_RETRIES = 2
+TASK_FILE = '201606231548.json'
+
+send_to_retry = {}
+
+with codecs.open(TASK_FILE, mode='r', encoding='utf-8') as file:
+    body = json.load(file)
+    print(body)
+
+
+def handle_504():
+    # Add 504s to retry dictionary
+    return
+
+
+def get_path_from_url(url):
+    # 1. Method that produces path from URL
+    # a. URL .replace(http base, mirror path) + index.html
+    return ''
+
+
+def generate_page_dictionary(page):
+    # 1. Take in URL list for each type
+    # 2. Construct URL/path dictionary
+    return {}
+
+
+def verify_files_exist(path):
+    # 3. Verify each path exists
+    # a. Send_to_retry
+    # b. sort by page
+    return {}
+
+
+#4 . Dictionary of page instances goes to size comparison
+    #a. Send to retry
+    #b. Add to dictionary to send to spot czech
+
+#5. Dictionary of passing page instance goes to spot czech
+    #a. Send to retry
+    #b. Log success!
+
+
+#1. Scrape
+#2. Save
+#3. Call verify steps 3 - end a set number of times
+
+
 # 2.
 
 # Notes:
 #  - we need to identify which mirror we're looking at
-
-# A - Initialize lists from task file (or whatever it's going to be)
-# B - Identify Problems
-#   1 - Existence Check
-#   2 - Size Comparison
-#   3 - Spot Check
-# C - Correct Problems (scrape all pages identified in B)
-# D - Repeat
-
-
-# temporary settings:
-NUM_RETRIES = 2
-
-with shelve.open('201606221420.task') as db:
-    node_list = [x.split('//', 1)[1] for x in db['node_urls']]
-    registration_list = db['registration_urls']
-    user_list = db['user_profile_page_urls']
-    institution_list = db['institution_urls']
-
