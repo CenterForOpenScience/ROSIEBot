e66a38eda52622730e1cf1a4e254075e98d8cdad
Matthew Keitelman <zamattiac@gmail.com>
2016-06-24 15:41:26 -0400

    Create OOP structured verification

---
+++ oop_verify.py
@@ -0,0 +1,79 @@
+import json, codecs
+import os
+import sys
+from bs4 import BeautifulSoup
+from settings import base_urls
+# from rescraper import Rescraper
+
+"""
+Pages engineered to fail:
+
+verify_page_exists: 3tmge/files/index.html
+size_comparison: 5dewf/files/index.html should be 340 KB
+"""
+
+# TODO: put this in settings
+NUM_RETRIES = 2
+TASK_FILE = '201606231548.json'
+MIRROR = '127.0.0.1/'
+
+
+with codecs.open(TASK_FILE, mode='r', encoding='utf-8') as file:
+    run_info = json.load(file)
+
+
+class Page:
+    def __init__(self, url, error=False):
+        self.url = url
+        self.error = error
+        if not error:
+            self.path = MIRROR + url.replace(base_urls[0], '') + 'index.html'
+            self.file_size = os.path.getsize(self.path)
+        else:
+            self.path = ''
+            self.file_size = 0
+
+    def __str__(self):
+        return self.path
+
+    def get_content(self):
+        name = file.split('/')[-3] + '/' + file.split('/')[-2]
+        soup = BeautifulSoup(open(file), 'html.parser')
+        return soup
+
+
+class Verifier:
+    def __init__(self):
+        self.pages = []
+        self.minimum_size = 0
+        self.page_elements = []
+        self.failed_pages = []
+
+    # First actual check
+    # Check that each file path in the dictionary actually exists
+    def verify_files_exist(self):
+        for page in self.pages:
+            print(page.path)
+            if not os.path.exists(page.path):
+                print('Failed: verify_files_exist(): ', page)
+                self.failed_pages.append(page)                                  # Add to naughty list
+                self.pages.pop(page)                                            # Remove from nice list
+        return
+
+    # Second check
+    # Compare page size to page-specific minimum that any fully-scraped page should have
+    def size_comparison(self):
+        for page in self.pages:
+            if not page.file_size > self.minimum_size:
+                print('Failed: size_comparison(): ', page, ' has size: ', page.file_size)
+                self.failed_pages.append(page)
+                self.pages.pop(page)
+        return
+
+    # Third check
+    # Check that specified elements are present and non-empty in each page
+    def spot_check(self):
+        for page in self.pages:
+            pass
+        return
+
--- verifier.py
+++ verifier.py
@@ -4,6 +4,13 @@ import sys
 from settings import base_urls
 # from rescraper import Rescraper
 
+"""
+Pages engineered to fail:
+
+verify_page_exists: 3tmge/files/index.html
+size_comparison: 5dewf/files/index.html should be 340 KB
+"""
+
 # TODO: put this in settings
 NUM_RETRIES = 2
 TASK_FILE = '201606231548.json'
@@ -42,7 +49,7 @@ class Verifier:
         self.json_list = []
         self.page = ''
         self.minimum_size = 0
-        self.page_elements = {}
+        self.page_elements = []
 
     # Takes a URL and produces its relative file name.
     def get_path_from_url(self, url):
@@ -57,24 +64,34 @@ class Verifier:
             if url.endswith(self.page + '/') and url not in run_info['error_list']:
                 key = self.get_path_from_url(url)
                 self.paths[key] = url
+                self.json_list.remove(url)
         return
 
+    # First actual check
     # Check that each file path in the dictionary actually exists
     def verify_files_exist(self):
         for path in self.paths:
+            print(path)
             if not os.path.exists(path):
                 print('Failed: verify_files_exist(): ', path)
-
-        # 3. Verify each path exists
-        # a. Send_to_retry
-        # b. sort by page
+                send_to_retry.append(self.paths[path])              # Add to naughty list
+                self.paths.pop(path)                                # Remove from nice list
         return
 
+    # Second check
+    # Compare page size to page-specific minimum that any fully-scraped page should have
     def size_comparison(self):
+        for path in self.paths:
+            file_size = os.path.getsize(path) / 1000                # in KB
+            if not file_size > self.minimum_size:
+                print('Failed: size_comparison(): ', path, ' has size: ', file_size)
+                send_to_retry.append(self.paths[path])
+                self.paths.pop(path)
         return
 
     def spot_check(self):
         return
+
 
 
 # Asynchronously re-download all the files that were unacceptable/missing
@@ -88,19 +105,18 @@ def rescrape():
 
 # Exectuion
 if run_info['scrape_finished']:
-    handle_errors()
+    handle_errors()         # Always run
     v = Verifier()
     v.page = 'files'
     v.json_list = run_info['node_urls']
+    v.minimum_size = 380
     v.generate_page_dictionary()
     v.verify_files_exist()
+    v.size_comparison()
     print(v.paths)
-
 
     for i in range(NUM_RETRIES):
         if run_info['scrape_nodes']:
-            if run_info['include_dashboard']:
-                pass
             if run_info['include_files']:
                 pass
             if run_info['include_wiki']:
@@ -110,6 +126,8 @@ if run_info['scrape_finished']:
             if run_info['include_registrations']:
                 pass
             if run_info['include_forks']:
+                pass
+            if run_info['include_dashboard']:       # This must go last because its URLs don't have a specific ending.
                 pass
             pass
         if run_info['scrape_registrations']:
