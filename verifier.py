cec35a5ef8e1046214432072ab9a13abcb11057f
Sadiyah Faruk <sf2ne@virginia.edu>
2016-06-29 16:22:05 -0400

    try this?

--- verifier.py
+++ verifier.py
@@ -26,6 +26,7 @@ MIRROR = 'archive/'
 
 with codecs.open(TASK_FILE, mode='r', encoding='utf-8') as file:
     run_info = json.load(file)
+with codecs.open(TASK_FILE, mode='r', encoding='utf-8') as file:
     run_copy = json.load(file)
 
 
