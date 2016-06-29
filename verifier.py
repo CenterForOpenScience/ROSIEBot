e0b216faf5d0c91df4cef604b5959c5e894ebe37
Sadiyah Faruk <sf2ne@virginia.edu>
2016-06-29 15:26:53 -0400

    Fixing problems found by Matt's CR

--- verifier.py
+++ verifier.py
@@ -400,6 +400,7 @@ def main():
                 if run_info['include_dashboard']:  # This must go last because its URLs don't have a specific ending.
                     project_dashboards = ProjectDashboardVerifier().failed_pages
                     nodes_list_verified += project_dashboards
+                run_info['node_urls_verified'] = nodes_list_verified
             if run_info['scrape_registrations']:
                 # Must run all page types automatically
                 registration_files = RegistrationFilesVerifier().failed_pages
@@ -409,16 +410,15 @@ def main():
                 registration_dashboards = RegistrationDashboardVerifier().failed_pages
                 registrations_list_verified = registration_files + registration_wiki + registration_analytics + \
                     registration_forks + registration_dashboards
+                run_info['registration_urls_verified'] = registrations_list_verified
             if run_info['scrape_users']:
                 user_profiles_verified = UserProfileVerifier().failed_pages
+                run_info['user_profile_page_urls_verified'] = user_profiles_verified
             if run_info['scrape_institutions']:
                 institution_dashboards_verified = InstitutionDashboardVerifier().failed_pages
+                run_info['institution_urls_verified'] = institution_dashboards_verified
 
             # clearing json file and dumping in new lists
-            run_info['node_urls_verified'] = nodes_list_verified
-            run_info['registration_urls_verified'] = registrations_list_verified
-            run_info['user_profile_page_urls_verified'] = user_profiles_verified
-            run_info['institution_urls_verified'] = institution_dashboards_verified
             with codecs.open(TASK_FILE, mode='w', encoding='utf-8') as file:
                 json.dump(run_info, file)
 
@@ -427,14 +427,14 @@ def main():
         second_chance = Crawler()
 
         if run_info['scrape_nodes']:
-            second_chance.node_urls = run_info['node_urls']
+            second_chance.node_urls = run_info['node_urls_verified']
             second_chance.scrape_nodes()
         if run_info['scrape_registrations']:
-            second_chance.registration_urls = run_info['registration_urls']
+            second_chance.registration_urls = run_info['registration_urls_verified']
         if run_info['scrape_users']:
-            second_chance.user_profile_page_urls = run_info['user_profile_page_urls']
+            second_chance.user_profile_page_urls = run_info['user_profile_page_urls_verified']
         if run_info['scrape_institutions']:
-            second_chance.institution_urls = run_info['institution_urls']
+            second_chance.institution_urls = run_info['institution_urls_verified']
 
 
 if __name__ == "__main__": main()
