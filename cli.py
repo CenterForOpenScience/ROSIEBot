1190aa9f35c05a2daac89664b12004972e049cde
adlius <adlius45@hotmail.com>
2016-07-01 13:14:47 -0400

    Clean up

--- cli.py
+++ cli.py
@@ -66,7 +66,7 @@ def cli_entry_point(scrape, resume, verify, compile_active, dm, tf, rn, registra
         try:
             verify_mirror(tf, rn)
         except FileNotFoundError:
-            click('File Not Found for the task.')
+            click.echo('File Not Found for the task.')
         return
 
     return
