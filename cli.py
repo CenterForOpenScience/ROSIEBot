b26fde51f1604faf5d8109f1a4cbb823809f46f3
Sadiyah Faruk <sf2ne@virginia.edu>
2016-07-18 10:27:30 -0400

    Wrote deletion feature for the mirror. Also contains CLI implementation.

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ import crawler
 import json
 import codecs
 import verifier
+import deleter
 
 @click.command()
 # Specify parameters that choose between different modes
@@ -13,10 +14,15 @@ import verifier
 @click.option('--verify', is_flag=True, help="Verify the existing OSF static mirror, need to import task file")
 @click.option('--resume_verify', is_flag=True, help="Resume verification of the existing OSF static mirror, need to "
                                                     "import task file")
+@click.option('--delete', is_flag=True, help="Delete nodes from the mirror that have been deleted by users")
 # Specify parameters for other needed values
 @click.option('--dm', default=None, type=click.STRING, help="Date marker needed for normal scrape")
 @click.option('--tf', default=None, type=click.STRING, help="filename of the task file")
 @click.option('--rn', default=3, type=click.INT, help="Number of times to retry")
+@click.option('--ptf', default=None, type=click.STRING, help="json file generated from compile_active of previously "
+                                                             "active nodes")
+@click.option('--ctf', default=None, type=click.STRING, help="json file generated from compile_active of currently "
+                                                             "active nodes")
 # Specify areas of scraping
 @click.option('--registrations', is_flag=True, help="Add this flag if you want to scrape for registrations")
 @click.option('--users', is_flag=True, help="Add this flag if you want to scrape for users")
@@ -29,13 +35,13 @@ import verifier
 @click.option('-a', is_flag=True, help="Add this flag if you want to include analytics page for nodes")
 @click.option('-r', is_flag=True, help="Add this flag if you want to include registrations page for nodes")
 @click.option('-k', is_flag=True, help="Add this flag if you want to include forks page for nodes")
-def cli_entry_point(scrape, resume, verify, resume_verify, compile_active, dm, tf, rn, registrations, users,
+def cli_entry_point(scrape, resume, verify, resume_verify, compile_active, delete, dm, tf, rn, ptf, ctf, registrations, users,
                     institutions, nodes, d, f, w, a, r, k):
     if scrape and resume and verify and compile_active:
         click.echo('Invalid parameters.')
         return
 
-    if not scrape and not resume and not verify and not resume_verify and not compile_active:
+    if not scrape and not resume and not verify and not resume_verify and not compile_active and not delete:
         click.echo('You have to choose a mode to run')
 
     if compile_active:
@@ -91,6 +97,13 @@ def cli_entry_point(scrape, resume, verify, resume_verify, compile_active, dm, t
         except FileNotFoundError:
             click.echo('File Not Found for the task.')
         return
+
+    if delete and ptf is not None and ctf is not None:
+        try:
+            delete_nodes(ptf, ctf)
+        except FileNotFoundError:
+            click.echo("Either the json file of previously active nodes was not found or the json file of currently "
+                       "active nodes was not found.")
 
     return
 
@@ -318,6 +331,19 @@ def resume_verify_mirror(tf, rn):
     else:
         for i in range(rn):
             verifier.main(tf, i)
+
+
+def delete_nodes(ptf, ctf):
+    with codecs.open(ptf, mode='r', encoding='utf-8') as previous_tf:
+        previous_task_file = json.load(previous_tf)
+    with codecs.open(ctf, mode='r', encoding='utf-8') as current_tf:
+        current_task_file = json.load(current_tf)
+    deleter.run_deleter(previous_task_file['list_of_active_registrations'],
+                        current_task_file['list_of_active_registrations'])
+    deleter.run_deleter(previous_task_file['list_of_active_users'],
+                        current_task_file['list_of_active_users'])
+    deleter.run_deleter(previous_task_file['list_of_active_nodes'],
+                        current_task_file['list_of_active_nodes'])
 
 if __name__ == '__main__':
     cli_entry_point()
---
+++ deleter.py
@@ -0,0 +1,57 @@
+# import statements should be here
+import shutil
+# import codecs
+# import json
+
+
+class Node:
+    def __init__(self, guid):
+        self.folder_path = self.get_path_from_guid(guid)
+
+    def get_path_from_guid(self, guid):
+        path = 'archive/' + guid
+        return path
+
+
+class Deletion:
+    def __init__(self, initial_list, subsequent_list):
+        self.previously_active_nodes = initial_list
+        self.currently_active_nodes = subsequent_list
+
+    def generate_nodes_list(self):
+        nodes_list = []
+        self.previously_active_nodes.sort()
+        for guid in self.previously_active_nodes:
+            print("Checking this guid: ", guid)
+            if guid not in self.currently_active_nodes:
+                node = Node(guid)
+                nodes_list.append(node)
+                print("Appending ", guid, " to nodes_list")
+                self.previously_active_nodes.remove(guid)
+        return nodes_list
+
+    def delete_node(self, node):
+        print("Deleting", node.folder_path)
+        shutil.rmtree(node.folder_path)
+
+
+def run_deleter(initial_list, subsequent_list):
+    print("inside run_deleter")
+    macc = Deletion(initial_list, subsequent_list)
+    print("Created macc")
+    nodes_list = macc.generate_nodes_list()
+    for node in nodes_list:
+        macc.delete_node(node)
+
+
+# def main(json1, json2):
+#     with codecs.open(json1, mode='r', encoding='utf-8') as previous_tf:
+#         previous_task_file = json.load(previous_tf)
+#     with codecs.open(json2, mode='r', encoding='utf-8') as current_tf:
+#         current_task_file = json.load(current_tf)
+#     run_deleter(previous_task_file['list_of_active_registrations'], current_task_file['list_of_active_registrations'])
+#     run_deleter(previous_task_file['list_of_active_users'], current_task_file['list_of_active_users'])
+#     run_deleter(previous_task_file['list_of_active_nodes'], current_task_file['list_of_active_nodes'])
+#
+# if __name__ == '__main__':
+#     main("list1.json", "list2.json")
