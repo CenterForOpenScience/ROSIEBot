189f431a109a2b07ac2affb219e73c7d8b1353fa
Matthew Keitelman <zamattiac@gmail.com>
2016-08-04 09:26:29 -0400

    Increase play speed

--- scripts/configure_demo.sh
+++ scripts/configure_demo.sh
@@ -9,4 +9,4 @@ cp -r mst3k ../demo_archive/project
 cp -r 2qj6a ../demo_archive/profile
 echo 'Demo files configured'
 cd ..
-doitlive play ./scripts/demo.sh -s 200
+doitlive play ./scripts/demo.sh -s 300
