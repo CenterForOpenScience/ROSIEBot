fe516db61015f83e899600242850122bafdfe025
adlius <adlius45@hotmail.com>
2016-08-08 16:01:41 -0400

    add docstrings

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # The OSF website URL, and the API
-base_urls = ['https://osf.io/', 'https://api.osf.io/v2/']
+# base_urls = ['https://osf.io/', 'https://api.osf.io/v2/']
+base_urls = ['http://localhost/', 'http://localhost:8000/v2/']
 
 DEBUG_LOG_FILENAME = 'debug_log.txt'
 ERROR_LOG_FILENAME = 'error_log.txt'
