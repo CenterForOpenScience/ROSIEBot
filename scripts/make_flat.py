92fa9ad7af413c419e6f127b851ecdb771f31028
Matthew Keitelman <zamattiac@gmail.com>
2016-08-11 09:59:44 -0400

    Add profile to list and a warning

--- scripts/make_flat.py
+++ scripts/make_flat.py
@@ -1,7 +1,10 @@
 #!/bin/python
 """
-Make the organization of the mirror flat so that relative URLs cooperate
+Produces a flat_archive version of the archive without OSF category folders registration, profile, and project, so that relative URLs cooperate
+
 archive/project/mst3k -> archive/mst3k
+
+WARNING: Utilities will not work with a flattened archive 
 
 :param 1 in CLI: folder to flatten
 """
@@ -11,7 +14,7 @@ import sys
 
 mirror = 'archive/' if len(sys.argv) < 2 else sys.argv[1] + '/'
 
-inflated_directories = ['project/', 'registration/']
+inflated_directories = ['project/', 'registration/', 'profile/']
 
 print("Flattening:", mirror)
 
